c940e7bc6e8475c2a4034339c944fbffc5981caa
bartstachurski <bart.stachurski@gmail.com>
2019-08-08 07:57:41 -0400

    add production brewerydb link to index and add API key to application yml file and edit brewerydbsearches controller to loop through breweries provided providing names to index.json.jb but still need to coax out the other variables I want to show

--- app/controllers/api/brewery_db_searches_controller.rb
+++ app/controllers/api/brewery_db_searches_controller.rb
@@ -1,8 +1,15 @@
 class Api::BreweryDbSearchesController < ApplicationController
 
   def index
-    response = HTTP.get("https://sandbox-api.brewerydb.com/v2/search/geo/point", :params => {key: ENV["BREWERY_DB_KEY"], lat: params[:lat], lng: params[:lng]})
-    @breweries = response.parse["data"][0]["brewery"]
+    # sandbox path:
+    # response = HTTP.get("https://sandbox-api.brewerydb.com/v2/search/geo/point", :params => {key: ENV["BREWERY_DB_KEY_SANDBOX"], lat: params[:lat], lng: params[:lng]})
+
+    # production path:
+    # response = HTTP.get("https://api.brewerydb.com/v2/search/geo/point", :params => {key: ENV["BREWERY_DB_KEY_PRODUCTION"], lat: params[:lat], lng: params[:lng]})
+
+    # production testing path to not use Google API requests up:
+    response = HTTP.get("https://api.brewerydb.com/v2/search/geo/point", :params => {key: ENV["BREWERY_DB_KEY_PRODUCTION"], lat: 39.9525839, lng: -75.16522150000003})
+    @breweries = response.parse["data"]
     render 'index.json.jb'
   end
 end
