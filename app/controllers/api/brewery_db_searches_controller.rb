70b2c85a0dadbe6cea0d0ec10fb89fd1689a0f91
bartstachurski <bart.stachurski@gmail.com>
2019-08-11 16:35:22 -0400

    edit brewery search index action to show specific brewery name and overall brewery name for location

--- app/controllers/api/brewery_db_searches_controller.rb
+++ app/controllers/api/brewery_db_searches_controller.rb
@@ -8,7 +8,7 @@ class Api::BreweryDbSearchesController < ApplicationController
     # response = HTTP.get("https://api.brewerydb.com/v2/search/geo/point", :params => {key: ENV["BREWERY_DB_KEY_PRODUCTION"], lat: params[:lat], lng: params[:lng]})
 
     # production testing path to not use Google API requests up:
-    response = HTTP.get("https://api.brewerydb.com/v2/search/geo/point", :params => {key: ENV["BREWERY_DB_KEY_PRODUCTION"], lat: 39.9525839, lng: -75.16522150000003})
+    response = HTTP.get("https://api.brewerydb.com/v2/search/geo/point", :params => {key: ENV["BREWERY_DB_KEY_PRODUCTION"], lat: params[:lat], lng: params[:lng]})
     @breweries = response.parse["data"]
     render 'index.json.jb'
   end
