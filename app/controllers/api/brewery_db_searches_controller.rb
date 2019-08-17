caac079d2dc816d55f49f47eb659fbeab550a107
bartstachurski <bart.stachurski@gmail.com>
2019-08-17 11:32:00 -0400

    add link to brewery db search show action to take first index of untappd brewery search based on brewery DB listing of breweries

--- app/controllers/api/brewery_db_searches_controller.rb
+++ app/controllers/api/brewery_db_searches_controller.rb
@@ -16,7 +16,15 @@ class Api::BreweryDbSearchesController < ApplicationController
   def show
     response = HTTP.get("https://api.brewerydb.com/v2/location/#{params[:id]}", :params => {key: ENV["BREWERY_DB_KEY_PRODUCTION"]})
     @brewery = response.parse["data"]
+    # untappd_response = HTTP.get("https://api.untappd.com/v4/search/brewery/?client_id=#{ENV["UNTAPPD_CLIENT_ID"]}&client_secret=#{ENV["UNTAPPD_CLIENT_SECRET"]}&q=#{@brewery["brewery"]["name"]}")
+    @untappd_name = URI.escape("Tired Hands Brewing Company")
+    untappd_find_brewery = HTTP.get("https://api.untappd.com/v4/search/brewery?client_id=#{ENV["UNTAPPD_CLIENT_ID"]}&client_secret=#{ENV["UNTAPPD_CLIENT_SECRET"]}&q=#{@untappd_name}")
+    @untappd_response = untappd_find_brewery.parse["response"]["brewery"]["items"][0]
+    # @untappd_response = untappd_response.parse["data"][0]
     # beers_response = HTTP.get("https://api.brewerydb.com/v2/brewery/#{@brewery.breweryId}", :params => {key: ENV["BREWERY_DB_KEY_PRODUCTION"]})
     render 'show.json.jb'
   end
 end
+
+ENV["UNTAPPD_CLIENT_ID"]
+ENV["UNTAPPD_CLIENT_SECRET"]
