94c4985dd0b6870718cf06daff3d70fe5ae6a0cd
bartstachurski <bart.stachurski@gmail.com>
2019-08-18 19:35:43 -0400

    amend untappd venue index view to show categories in array to filter results for breweries on front end with vue

--- app/controllers/api/untappd_venues_controller.rb
+++ app/controllers/api/untappd_venues_controller.rb
@@ -1,8 +1,8 @@
 class Api::UntappdVenuesController < ApplicationController
   def index
     venues = HTTP.get("https://api.untappd.com/v4/search/venue?client_id=#{ENV["UNTAPPD_CLIENT_ID"]}&client_secret=#{ENV["UNTAPPD_CLIENT_SECRET"]}&q=Ardmore")
+    # @venues = venues.parse["response"]["venues"]["items"]
     @venues = venues.parse["response"]["venues"]["items"]
-    @venues = @venues.select {|venue| venue["venue"]["categories"]["items"]["category_name"].downcase == "brewery"}
     render 'index.json.jb'
   end
 
