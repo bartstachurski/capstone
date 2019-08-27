24b0b58d9fe03c703bcef9b6e1dba3cd364151f5
bartstachurski <bart.stachurski@gmail.com>
2019-08-26 21:19:06 -0400

    add foursquare api show page action and view

--- app/controllers/api/foursquare_venues_controller.rb
+++ app/controllers/api/foursquare_venues_controller.rb
@@ -5,8 +5,8 @@ class Api::FoursquareVenuesController < ApplicationController
     @foursquare_breweries = foursquare_breweries.parse["response"]["groups"][0]["items"]
     @foursquare_breweries.each do |brewery|
       untappd_convert_id_response = HTTP.get("https://api.untappd.com/v4/venue/foursquare_lookup/#{brewery["venue"]["id"]}?client_id=#{ENV["UNTAPPD_CLIENT_ID"]}&client_secret=#{ENV["UNTAPPD_CLIENT_SECRET"]}")
-      untappd_id = untappd_convert_id_response.parse["response"]["venue"]["items"][0]["venue_id"]
-      brewery["venue"]["untappd_id"] = untappd_id
+      untappd_venue_id = untappd_convert_id_response.parse["response"]["venue"]["items"][0]["venue_id"]
+      brewery["venue"]["untappd_venue_id"] = untappd_venue_id
     end
     # @foursquare_breweries.each do |brewery|
     # foursquare_venue_id = @foursquare_breweries[0]["venue"]["id"]
@@ -18,6 +18,8 @@ class Api::FoursquareVenuesController < ApplicationController
   end
 
   def show
+    foursquare_venue_info = HTTP.get("https://api.foursquare.com/v2/venues/4fc99910bb3d84c733701efb?client_id=#{ENV["FOURSQUARE_CLIENT_ID"]}&client_secret=#{ENV["FOURSQUARE_CLIENT_SECRET"]}&v=20180323")
+    @foursquare_venue_info = foursquare_venue_info.parse["response"]["venue"]
     render 'show.json.jb'
   end
 end
