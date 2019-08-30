74e631db46fd2d4840c75b99bed36f5249cdd12e
bartstachurski <bart.stachurski@gmail.com>
2019-08-30 17:16:13 -0400

    moved the friend requests controller to the api folder and fleshed out a lot of the friend request controller

---
+++ app/controllers/api/friend_requests_controller.rb
@@ -0,0 +1,44 @@
+class Api::FriendRequestsController < ApplicationController
+  # before_action :set_friend_request, except: [:index, :create]
+
+  def create
+    friend = User.find(params[:friend_id])
+    @friend_request = current_user.friend_requests.new(friend: friend)
+    @friend_request.save
+
+    if @friend_request.save
+      render 'show.json.jb'
+    else
+      render json: @friend_request.errors, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @incoming = FriendRequest.where(friend: current_user)
+    @outgoing = current_user.friend_requests
+  end
+
+  def update
+    @friend_request = FriendRequest.find_by(id: params[:id])
+    if params[:accepted] == true
+      @friend_request.accept
+      render json: "request accepted"
+    else
+      render json: "failed to accept"
+    end
+  end
+
+  def destroy
+    @friend_request = FriendRequest.find_by(id: params[:id])
+    @friend_request.destroy
+    render 'destroy.json.jb'
+    # head :no_content
+  end
+  # ...
+  # private
+
+  # def set_friend_request
+  #   @friend_request = FriendRequest.find(params[:id])
+  # end
+
+end
--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,30 +1,32 @@
-class FriendRequestsController < ApplicationController
-  before_action :set_friend_request, except: [:index, :create]
+# class FriendRequestsController < ApplicationController
+#   # before_action :set_friend_request, except: [:index, :create]
 
-  def create
-    friend = User.find(params[:friend_id])
-    @friend_request = current_user.friend_requests.new(friend: friend)
+#   def create
+#     friend = User.find(params[:friend_id])
+#     @friend_request = current_user.friend_requests.new(friend: friend)
 
-    if @friend_request.save
-      render :show, status: :created, location: @friend_request
-    else
-      render json: @friend_request.errors, status: :unprocessable_entity
-    end
-  end
+#     if @friend_request.save
+#       render :show, status: :created, location: @friend_request
+#     else
+#       render json: @friend_request.errors, status: :unprocessable_entity
+#     end
+#   end
 
-  def index
-    @incoming = FriendRequest.where(friend: current_user)
-    @outgoing = current_user.friend_requests
-  end
+#   def index
+#     @incoming = FriendRequest.where(friend: current_user)
+#     @outgoing = current_user.friend_requests
+#   end
 
-  def destroy
-    @friend_request.destroy
-    head :no_content
-  end
-  ...
-  private
+#   def destroy
+#     print "hello from destroy"
+#     friend_request = FriendRequest.find_by(id: params[:id])
+#     @friend_request.destroy
+#     head :no_content
+#   end
+#   # ...
+#   # private
 
-  def set_friend_request
-    @friend_request = FriendRequest.find(params[:id])
-  end
-end
+#   # def set_friend_request
+#   #   @friend_request = FriendRequest.find(params[:id])
+#   # end
+# end
