b2efab07050dcaed0089093794c29718dab8a100
bartstachurski <bart.stachurski@gmail.com>
2019-09-03 16:26:39 -0400

    fix bug on homne page when searching that if foursquare does not automatically include the brewery id, the code will default to the brewery id on the first checkin for that venue

--- app/controllers/api/untappd_venues_controller.rb
+++ app/controllers/api/untappd_venues_controller.rb
@@ -9,6 +9,13 @@ class Api::UntappdVenuesController < ApplicationController
     untappd_venue_info = HTTP.get("https://api.untappd.com/v4/venue/info/#{params[:id]}?client_id=#{ENV["UNTAPPD_CLIENT_ID"]}&client_secret=#{ENV["UNTAPPD_CLIENT_SECRET"]}")
     @untappd_venue_info = untappd_venue_info.parse["response"]["venue"]
     @sorted_brewery_locations = @untappd_venue_info["brewery_locations"]["items"].sort_by{ |brewery| brewery["brewery_id"]}
+    if @sorted_brewery_locations == []
+      @brewery_id = @untappd_venue_info["media"]["items"][0]["brewery"]["brewery_id"]
+      @brewery_name = @untappd_venue_info["media"]["items"][0]["brewery"]["brewery_name"]
+    else
+      @brewery_id = @sorted_brewery_locations[0]["brewery_id"]
+      @brewery_name = @sorted_brewery_locations[0]["brewery_name"]
+    end
     render 'show.json.jb'
   end
 end
