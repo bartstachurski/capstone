675a0e70053e4647e3fe3ae4dc9ca363c1fe3768
Bart Stachurski <bart.stachurski@gmail.com>
2020-11-11 22:24:29 -0500

    installs pry and fixes friendships create action and fetching secret key base

--- app/controllers/api/friendships_controller.rb
+++ app/controllers/api/friendships_controller.rb
@@ -2,10 +2,7 @@ class Api::FriendshipsController < ApplicationController
   def create
     @user = current_user
     @new_friend = User.find_by(id: params[:user_id])
-    @new_friend.friends << @user
     @user.friends << @new_friend
-    @user.save
-    @new_friend.save
     render 'create.json.jb'
   end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
       begin
         decoded_token = JWT.decode(
           token,
-          Rails.application.credentials.fetch(:secret_key_base),
+          Rails.application.secret_key_base,
           true,
           { algorithm: 'HS256' }
         )
